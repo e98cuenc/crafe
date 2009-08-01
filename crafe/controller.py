b22db9a9096cb2b8c2b94a50473d56cb61ee2484
Joaquin Cuenca Abela <e98cuenc@gmail.com>
2009-08-01 19:57:33 +0200

    Serve static content from webpy
    Serve directly static content from webpy. Useful to develop without NGINX.

--- crafe/controller.py
+++ crafe/controller.py
@@ -3,8 +3,11 @@ import web
 import os
 from web.contrib.template import render_jinja
 
+import configuration
+
 urls = ( '/', 'GenericPage',
-        r'/(hello)', 'GenericPage',)
+        r'/(css|js|img)/(.*)', 'StaticFile',
+        r'/favicon.ico', 'StaticFavicon')
 
 app = web.application(urls, globals())
 base_render = render_jinja('templates', extensions = ['jinja2.ext.i18n'],
@@ -15,19 +18,55 @@ base_render = render_jinja('templates', extensions = ['jinja2.ext.i18n'],
                                'head_description': _('Description of Crafe'),
                                'js_srcs': [],
                                'css_srcs': [],
+                               'version': configuration.STATIC_CONTENT_VERSION,
                            })
 
 # Used for Jinja2 i18n extension.
 curdir = os.path.abspath(os.path.dirname(__file__))
-localedir = curdir + '/i18n'
+localedir = os.path.join(curdir, 'i18n')
 # TODO: Choose the right language based on cookies / Accept-Language
 lang = gettext.translation('messages', localedir, languages=['en_US'])
 base_render._lookup.install_gettext_translations(lang)
 
 
 class GenericPage:
+    """Renders a page that uses base_render template."""
+
     def GET(self, page_name='index'):
         return getattr(base_render, page_name)({'page_name': page_name})
+
+
+class StaticFile:
+    """Serve a static file.
+    
+    This method should not be used in production, you should instead serve the
+    file from the frontend webserver (nginx). It's however useful to emulate
+    this for development without nginx.
+    """
+
+    def GET(self, dirname, filename):
+        supported_content_types = {
+            'css': 'text/css',
+            'js': 'text/javascript',
+            'ico': 'image/ico',
+            'png': 'image/png',
+            'jpg': 'image/jpeg',
+            'gif': 'image/gif',
+        }
+        content_type = 'application/octet-stream'
+        dot_position = filename.rfind('.')
+        if dot_position >= 0:
+            file_extension = filename[dot_position + 1:]
+            if file_extension in supported_content_types:
+                content_type = supported_content_types[file_extension]
+
+        web.header('Content-Type', content_type)
+        return open(os.path.join('static', dirname, filename), 'rb').read()
+
+
+class StaticFavicon(StaticFile):
+    def GET(self):
+        return StaticFile.GET(self, 'img', 'favicon.ico')
 
 
 if __name__ == "__main__":
