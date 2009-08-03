910ef57c47950e23d09c2f79e636cdea6ab16521
Joaquin Cuenca Abela <e98cuenc@gmail.com>
2009-08-03 22:06:04 +0200

    Add more tests
    Test the CSS and JS files.

--- crafe/controller_test.py
+++ crafe/controller_test.py
@@ -1,6 +1,7 @@
 import controller
 from nose import tools
 from paste import fixture
+import logging
 
 
 def setup():
@@ -10,8 +11,22 @@ def teardown():
     pass
 
 def test_index():
-    middleware = []
-    app = fixture.TestApp(controller.app.wsgifunc(*middleware))
+    app = fixture.TestApp(controller.app.wsgifunc())
     r = app.get('/')
     tools.assert_equal(r.status, 200)
+    tools.assert_equal(r.header('Content-Type'), 'text/html')
     # r.mustcontain('foo')
+
+def test_css():
+    app = fixture.TestApp(controller.app.wsgifunc())
+    for css_url in ['/css/screen.css']:
+        r = app.get(css_url)
+        tools.assert_equal(r.status, 200)
+        tools.assert_equal(r.header('Content-Type'), 'text/css')
+
+def test_js():
+    app = fixture.TestApp(controller.app.wsgifunc())
+    for js_url in ['/js/index.js']:
+        r = app.get(js_url)
+        tools.assert_equal(r.status, 200)
+        tools.assert_equal(r.header('Content-Type'), 'text/javascript')
